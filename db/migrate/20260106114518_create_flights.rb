56be18e64c5edccac88189ea93034eba9a274f09
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-01-06 19:45:54 +0800

    generate flight & airport model

---
+++ db/migrate/20260106114518_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[8.0]
+  def change
+    create_table :flights do |t|
+      t.integer :departure_airport
+      t.integer :arrival_airport
+      t.datetime :start
+      t.integer :duration_minutes
+
+      t.timestamps
+    end
+  end
+end
