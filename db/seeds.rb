97a4797873c3b4fa12e92e5bf121f3b108d54620
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-01-07 01:34:24 +0800

    seed flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[ "LAX", "JFK", "SFO" ].each do |airport|
+  Airport.find_or_create_by!(code: airport)
+end
+
+LONG_FLIGHT_START_TIMES=[ 9, 17 ]
+SHORT_FLIGHT_START_TIMES=[ 8, 12, 16, 20 ]
+
+def get_time(hours)
+  Time.zone.parse("2027-01-01 #{hours<10?"0#{hours}":hours}:00:00")
+end
+
+LONG_FLIGHT_START_TIMES.each do |start|
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK").id, arrival_airport: Airport.find_by(code: "LAX").id, start: get_time(start), duration_minutes: 300)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX").id, arrival_airport: Airport.find_by(code: "JFK").id, start: get_time(start), duration_minutes: 300)
+
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK").id, arrival_airport: Airport.find_by(code: "SFO").id, start: get_time(start), duration_minutes: 360)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO").id, arrival_airport: Airport.find_by(code: "JFK").id, start: get_time(start), duration_minutes: 360)
+end
+
+
+
+SHORT_FLIGHT_START_TIMES.each do |start|
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO").id, arrival_airport: Airport.find_by(code: "LAX").id, start: get_time(start), duration_minutes: 120)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX").id, arrival_airport: Airport.find_by(code: "SFO").id, start: get_time(start), duration_minutes: 120)
+end
