45d9282cfe7e5cb75508f92ba0aa0bdf94de9980
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-01-10 02:17:14 +0800

    add day 2 & refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,21 +15,28 @@ end
 LONG_FLIGHT_START_TIMES=[ 9, 17 ]
 SHORT_FLIGHT_START_TIMES=[ 8, 12, 16, 20 ]
 
-def get_time(hours)
-  Time.zone.parse("2027-01-01 #{hours<10?"0#{hours}":hours}:00:00")
+def get_time(day, hours)
+  Time.zone.parse("#{day} #{hours<10?"0#{hours}":hours}:00:00")
 end
 
-LONG_FLIGHT_START_TIMES.each do |start|
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK").id, arrival_airport: Airport.find_by(code: "LAX").id, start: get_time(start), duration_minutes: 300)
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX").id, arrival_airport: Airport.find_by(code: "JFK").id, start: get_time(start), duration_minutes: 300)
+LONG_FLIGHT_START_TIMES.each do |start_time|
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK"), arrival_airport: Airport.find_by(code: "LAX"), start: get_time("2027-01-01", start_time), duration_minutes: 300)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX"), arrival_airport: Airport.find_by(code: "JFK"), start: get_time("2027-01-01", start_time), duration_minutes: 300)
 
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK").id, arrival_airport: Airport.find_by(code: "SFO").id, start: get_time(start), duration_minutes: 360)
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO").id, arrival_airport: Airport.find_by(code: "JFK").id, start: get_time(start), duration_minutes: 360)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK"), arrival_airport: Airport.find_by(code: "SFO"), start: get_time("2027-01-01", start_time), duration_minutes: 360)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO"), arrival_airport: Airport.find_by(code: "JFK"), start: get_time("2027-01-01", start_time), duration_minutes: 360)
+
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK"), arrival_airport: Airport.find_by(code: "LAX"), start: get_time("2027-01-02", start_time+1), duration_minutes: 300)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX"), arrival_airport: Airport.find_by(code: "JFK"), start: get_time("2027-01-02", start_time+1), duration_minutes: 300)
+
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "JFK"), arrival_airport: Airport.find_by(code: "SFO"), start: get_time("2027-01-02", start_time+1), duration_minutes: 360)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO"), arrival_airport: Airport.find_by(code: "JFK"), start: get_time("2027-01-02", start_time+1), duration_minutes: 360)
 end
 
+SHORT_FLIGHT_START_TIMES.each do |start_time|
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO"), arrival_airport: Airport.find_by(code: "LAX"), start: get_time("2027-01-01", start_time), duration_minutes: 120)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX"), arrival_airport: Airport.find_by(code: "SFO"), start: get_time("2027-01-01", start_time), duration_minutes: 120)
 
-
-SHORT_FLIGHT_START_TIMES.each do |start|
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO").id, arrival_airport: Airport.find_by(code: "LAX").id, start: get_time(start), duration_minutes: 120)
-  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX").id, arrival_airport: Airport.find_by(code: "SFO").id, start: get_time(start), duration_minutes: 120)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "SFO"), arrival_airport: Airport.find_by(code: "LAX"), start: get_time("2027-01-02", start_time+1), duration_minutes: 120)
+  Flight.find_or_create_by!(departure_airport: Airport.find_by(code: "LAX"), arrival_airport: Airport.find_by(code: "SFO"), start: get_time("2027-01-02", start_time+1), duration_minutes: 120)
 end
