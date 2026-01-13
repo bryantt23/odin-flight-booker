1158f1870bcf07e597051301411e765eadc1833e
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-01-14 00:35:11 +0800

    show search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,17 @@ class FlightsController < ApplicationController
   def index
     @flights=Flight.all
     @airports=Airport.all
-    @flight_dates=Flight.all.map{|f|f.start.strftime("%m/%d/%Y")}.uniq
+    @flight_dates=Flight.all.map { |f|f.start.strftime("%m/%d/%Y") }.uniq
+    @url=request.original_url
+
+    puts request.original_url
+    puts "arrival_airport #{params[:arrival_airport]}"
+    puts "passengers #{params[:passengers]}"
+    @params=params
+    @date=Date.strptime(params[:date], "%m/%d/%Y")
+    puts @date
+    @search_results=Flight.where(departure_airport: params[:departure_airport], arrival_airport: params[:arrival_airport], start: @date.beginning_of_day..@date.end_of_day)
+      puts @search_results
   end
 
   def create
