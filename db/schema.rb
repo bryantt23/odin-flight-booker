87e6aae2f031594cccd208cad67acac1212ccb78
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-01-21 07:29:19 +0800

    update passenger & booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_20_190504) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_20_232849) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_20_190504) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -35,6 +36,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_20_190504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "booking_id", null: false
+    t.string "name"
     t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
