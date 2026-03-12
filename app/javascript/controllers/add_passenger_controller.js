06c4c880929137fe9160e25a75e7f76016373c21
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-03-12 02:07:28 -0700

    delete unless last one

--- app/javascript/controllers/add_passenger_controller.js
+++ app/javascript/controllers/add_passenger_controller.js
@@ -3,19 +3,35 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["items"]
 
-    connect() {
-        // this.element.textContent = "Hello World from passenger!!!"
+    deleteRow(e) {
+        e.preventDefault()
+        console.log(this.parentNode)
+        const passengerFields = this.parentNode.parentNode.parentNode.querySelectorAll(".passenger-field")
+        if (passengerFields.length > 1) {
+            this.parentNode.remove()
+        }
     }
 
+    connect() {
+        // this.element.textContent = "Hello World from passenger!!!"
+        console.log(this.itemsTarget)
+        // debugger
+        const passengerFields = this.itemsTarget.parentNode.querySelectorAll(".passenger-field")
+        for (const field of passengerFields) {
+            const button = document.createElement("button")
+            button.addEventListener('click', this.deleteRow)
+            button.innerText = "Delete"
+            field.appendChild(button)
+        }
+
+    }
 
     addItem(e) {
         e.preventDefault();
-        const item = document.createElement('li')
-        const breakElem = document.createElement("br")
-        item.innerText = `hi ${Date.now()}`
-
 
         // 1. Create the input element
+        const div = document.createElement("div")
+        div.className = "passenger-field"
         const inputField = document.createElement('input')
         inputField.setAttribute('type', 'text')
         inputField.setAttribute('name', `booking[passengers_attributes][${Date.now()}][name]`)
@@ -27,9 +43,15 @@ export default class extends Controller {
         // 3. Associate the label with the input using the htmlFor property
         labelElement.htmlFor = 'name'
 
+        //delete button
+        const button = document.createElement("button")
+        button.addEventListener('click', this.deleteRow)
+        button.innerText = "Delete"
+
         // 4. Append both to the document body (or another container)
-        this.itemsTarget.appendChild(labelElement)
-        this.itemsTarget.appendChild(inputField)
-        this.itemsTarget.appendChild(breakElem)
+        div.appendChild(labelElement)
+        div.appendChild(inputField)
+        div.appendChild(button)
+        this.itemsTarget.appendChild(div)
     }
 }
